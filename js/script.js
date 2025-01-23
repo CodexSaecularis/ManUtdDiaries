95adaeea231f3cfad6b803987befa068a73138a7
CodexSaecularis <idierco@gmail.com>
2025-01-23 03:46:15 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@
 // 	wrapper: '.wrapper',
 // 	content: '.content',
 // })
+
+// https://stackoverflow.com/questions/54142435/href-link-target-open-in-same-tab-in-mobile-view
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById("links").target = "_self";     //change target
+}
